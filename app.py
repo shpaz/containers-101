61380845d89650f10a082d23cca01d171c4df724
root <shonpaz123@gmail.com>
2022-03-07 14:52:22 -0500

    added initial config

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ from flask import request, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://mongodb:27017/movies"
+app.config["MONGO_URI"] = "mongodb://mongodb:27017/containers-101"
 mongo = PyMongo(app)
 
 @app.route("/")
 def home():
-    return "Welcome to Shon's PoC! nice To Meet You!"
+    return "Welcome to Containers 101 course! Nice to meet you!"
     
 @app.route("/list", methods=["GET"])
 def list_collection():
-    data = mongo.db.fiction.find()
+    data = mongo.db.inventory.find()
     output = [] 
     for line in data:
-      output.append({'name' : line['name']})
-    return jsonify({'result': output})
+      line['_id'] = str(line['_id'])
+      output.append(line)
+    return jsonify({'documents': output})
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
